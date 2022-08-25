d6fbc78a3e13cd9995a720f7b62d9f54f9504b63
Mohamed-ryad LAOUEDJ <mlaouedj@e1r9p20.clusters.42paris.fr>
2022-08-25 02:38:55 +0200

    containers_final_25/08/2022

--- Containers/Map.hpp
+++ Containers/Map.hpp
@@ -62,7 +62,7 @@ namespace ft
 
 			map& operator=(const map& obj) {}	//--->Operator =  to do
 
-//......................TREE FUNCTIONS
+////////////////////////PRIVATE TREE FUNCTIONS
 		private:
 			node_type*	new_node(const value_type& val, node_type *parent)
 			{
@@ -108,8 +108,152 @@ namespace ft
 				}
 				return insert_node(val, current, parent);
 			}
-			//..INSERT_NODE to do
-				
+
+			node_type*	insert_node(onst value_type& val, node_type* current, node_type* parent)
+			{
+				if (!current)
+					return new_node(val, parent);
+				if (current->last)
+				{
+					node_type* to_insert = new_node(val, parent);
+					current->parent = to_insert;
+					to_insert->right = current;
+					current = to_insert;
+					return current
+				}
+				if (_key_compare(val.first, current->value.first))
+					current->left = insert_node(val, current->left, current);
+				else if (_key_compare(current->value.first, val.first))
+					current->right = insert_node(val, current->right, current);
+				return current;
+			}
+
+			node_type*	delete_node(node_type* current, const key_type& key)
+			{
+				if (!current || current->last)
+					return current;
+				if (_key_compare(key, current->value.first))
+					current->left = delete_node(current->left, key);
+				else if (_key_compare(current->value.first, key))
+					current->right = delete_node(current->right, key);
+				else
+				{
+					if (!current->left || !current->right)
+					{
+						node_type* temp = current->left ? current->left : current->right;
+						if (!current->left && !current->right)
+						{
+							temp = current;
+							_alloc.destroy(temp);
+							_alloc.deallocate(temp, 1);
+							current = NULL;
+							_size--;
+						}
+						else
+						{
+							temp->parent = current->parent;
+							node_type* temp1 = current;
+							current = temp;
+							_alloc.destroy(temp1);
+							_alloc.deallocate(temp1, 1);
+							_size--;
+						}
+					}
+					else
+					{
+						node_type* temp = min_node(current->right);
+						if (temp != currrent->right)
+						{
+							temp->right = current->right;
+							current->right->parent = temp;
+						}
+						temp->left = current->left;
+						current->left->parent = temp;
+						temp->parent->left = NULL;
+						temp->parent = current->parent;
+						if (_root == current)
+							_root = temp;
+						_alloc.destroy(current);
+						_alloc.deallocate(current, 1);
+						_size--;
+						current =  temp;
+					}
+				}
+				return current;
+			}
+
+			void		clear_node(node_type* current)
+			{
+				if (current)
+				{
+					clear_node(current->left);
+					clear_node(current->right);
+					_alloc.destroy(current);
+					_alloc.deallocate(current, 1);
+					if (_size > 0)
+						_size--;
+					if (current == _root)
+						_root = NULL;
+				}
+					
+			}
+			
+			node_type*	find_key(const key_type& key, node_type* current)
+			{
+				if (!current || current->last)
+					return NULL;
+				if (_key_compare(key, current->value.first))
+					return find_key(key, current->left);
+				else if (_key_compare(current->value.first, key))
+					return find_key(key, current->right);
+				else
+					return current;
+			}
+
+			node_type*	const_find_key(const key_type& key, node_type* current) const
+			{
+				if (!current || current->last)
+					return NULL;
+				if (_key_compare(key, current->value.first))
+					return const_find_key(key, current->left);
+				else if (_key_compare(current->value.first, key))
+					return const_find_key(key, current->right);
+				else
+					return current;
+			}
+
+			void		tree_init(void)
+			{
+				insert(value_type(key_type(), mapped_type()));
+				_root->last = true;
+				_size--;
+			}
+////////////////////////PUBLIC CLASS METHODS
+//......................CAPACITY
+			bool		empty(void) const
+			{
+				return _size == 0;
+			}
+
+			size_type	size(void) const
+			{
+				return _size;
+			}
+
+			size_type	max_size(void) const
+			{
+				return _alloc.max_size();
+			}
+//......................OBSERVERS
+			key_compare	key_comp(void) const
+			{
+				return _key_compare;
+			}
+
+			value_compare	value_comp(void) const
+			{
+				return value_compare(key_compare());
+			}
 			
 
 
